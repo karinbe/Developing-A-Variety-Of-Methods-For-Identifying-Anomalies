9d2f5051c1c6fdcbae9920f48ad5ab1a1c684752
Hadas135 <hadasi135@gmail.com>
2018-05-25 16:30:01 +0300

    Add comments to  Entropy code & change LZ code in accordance

--- src/LZ.m
+++ src/LZ.m
@@ -7,7 +7,7 @@
 rng default; % For reproducibility
 
 % Import the data:
-[~, ~, raw] = xlsread('C:\Users\Hadas\Desktop\Diabetes.xls','Sheet1','A2:I769');
+[~, ~, raw] = xlsread('C:\Users\Hadas\Desktop\Developing-Variety-Of-Methods-For-Identifying-Anomalies-\tables\Diabeteswith01.xls','Sheet1','A2:I769');
 
 % Replace non-numeric cells with 0.0:
 R = cellfun(@(x) (~isnumeric(x) && ~islogical(x)) || isnan(x),raw); % Find non-numeric cells
@@ -18,46 +18,41 @@ dataAsNums = reshape([raw{:}],size(raw));
 
 % Find out data size:
 [rows, columns] = size(dataAsNums);
-
 % Quantization level -
 % Convert our data to string:
 dataAsStr = ''; % A string which hold the table's values
+trainingDataAsStr = ''; % A string which hold the training data's values
 arrRange = cell(1,5); % range of the different cell's types
 ascii = 97; % the small char 'a', helps to catalog cell's types
 index = 1; % indicator in arrArange
 
+n = 10; % Arbitrary selection of the amount of training data
+found = 0; % Training data - n first healthy
+
 for i = 1:rows
-    for j = 2:columns
-        number = dataAsNums(i,j);
-        if number >= 0 && number < 60 
-            index = 1;
-        elseif number >= 60 && number < 80
-            index = 2;
-        elseif number >= 80 && number < 150
-            index = 3;
-        elseif number >= 150 && number < 200
-            index = 4;
-        else
-            index = 5;
-        end
-            
+    for j = 1:columns-1
+        index = findIndexByRange(dataAsNums(i,j));
         if isempty(arrRange{index})
             arrRange{index} = ascii;
             ascii = ascii + 1;
         end
-         dataAsStr = strcat(dataAsStr,char(arrRange{index}));
+        
+        if found < n && dataAsNums(i,columns) == 1
+            trainingDataAsStr = strcat(trainingDataAsStr,char(arrRange{index}));
+        else
+            dataAsStr = strcat(dataAsStr,char(arrRange{index}));
+        end
+    end
+    if dataAsNums(i,columns) == 1
+        found = found + 1;
     end
 end
 
 disp(dataAsStr);
-
-[~, dataLength] = size(dataAsStr);
-
-LengthOfAllData = dataLength;
-dataLength = 10 * (columns - 1); % const - we arbitrarily chose 10 lines
-SearchStartIndex = dataLength + 1;
+disp(trainingDataAsStr);
 
 % Variables for LZ78 Algorithm:
+[~, dataLength] = size(trainingDataAsStr);
 dict = cell(1,dataLength); % Dictionary
 fatherLocation = zeros(1,dataLength);
 currentString = '';
@@ -65,7 +60,7 @@ currentDictIndex = 1;
 
 % Build the dictionary:
 for i = 1:dataLength
-    currentString = strcat(currentString,dataAsStr(i));
+    currentString = strcat(currentString,trainingDataAsStr(i));
     index = isFound(currentString, dict, currentDictIndex);
     if index == 0 % If currentString isn't in the dictionary
         dict(currentDictIndex) = {currentString};
@@ -107,19 +102,22 @@ for i = 1:dataLength
 end
 
 % Search:
-howManyTimes = (LengthOfAllData - SearchStartIndex + 1) / (columns - 1);% How many times the loop will run
-startPos = SearchStartIndex;
-endPos = SearchStartIndex + columns - 2;
+[~, testingDataLength] = size(dataAsStr);
+howManyTimes = testingDataLength / (columns - 1); % How many times the loop will run
+startPos = 1;
+endPos = columns - 1;
+
 for i = 1:howManyTimes
     stringToSearch = extractBetween(dataAsStr,startPos,endPos);
     if ~ismember(stringToSearch , lzTree)
-        disp(dataAsNums((startPos - 1)/(columns - 1) + 1, 1) + " ('" + stringToSearch + "') is Anomaly.");
+        p = (startPos - 1)/(columns - 1) + 1;
+        disp(p + ": '" + stringToSearch + "' is Anomaly.");
     end
     startPos = startPos + columns - 1;
     endPos = endPos + columns - 1;
 end
 
-
+clear;
 % -------------------------------------------------------------------------
 % Function that get a string, array of strings and the size of the array
 % and check if the string appears inside the array or not.
@@ -132,4 +130,18 @@ function fatherIndex = isFound(currentString, dict, currentDictIndex)
         end
     end
     fatherIndex = 0;
+end
+% -------------------------------------------------------------------------
+function index = findIndexByRange(number)
+    if number >= 0 && number < 60 
+        index = 1;
+    elseif number >= 60 && number < 80
+        index = 2;
+    elseif number >= 80 && number < 150
+        index = 3;
+    elseif number >= 150 && number < 200
+        index = 4;
+    else
+        index = 5;
+    end
 end
--- src/entropy.m
+++ src/entropy.m
@@ -1,7 +1,7 @@
 rng default; % For reproducibility
  
 % Import the data:
-[~, ~, raw] = xlsread('C:\Users\קארין\Desktop\Developing-Variety-Of-Methods-For-Identifying-Anomalies-\tables\Diabeteswith01.xls','Sheet1','A2:I769');
+[~, ~, raw] = xlsread('C:\Users\Hadas\Desktop\Developing-Variety-Of-Methods-For-Identifying-Anomalies-\tables\Diabeteswith01.xls','Sheet1','A2:I769');
  
  R = cellfun(@(x) (~isnumeric(x) && ~islogical(x)) || isnan(x),raw); % Find non-numeric cells
  raw(R) = {2.0}; % Replace non-numeric cells
@@ -12,14 +12,15 @@ data = reshape([raw{:}],size(raw));
 % Find out data size:
 [rows, columns] = size(data);
  
-maxColumnsArray = zeros(1, columns-1);
+maxColumnsArray = zeros(1, columns-1); % Contains the highest value of each column
 for i = 1:columns-1
     maxVal = max(data(:,i));
     maxColumnsArray(i) = maxVal;
 end
  
-Hx = zeros(1,rows);
- 
+Hx = zeros(1,rows); % Contains the entropy of each row
+
+% Calculating entropy based on the most influential columns:
 for i = 1:rows
   number = data(i,2);
   number2 = data(i,5);
@@ -27,79 +28,81 @@ for i = 1:rows
   Hx(i) = (px * log10(1/px));
 end
  
-counterSS=0;
-counterHS=0;
-counterSH=0;
-counterHH=0;
+% Counting our success and failure
+% The counters in the following format: counter<sick or healthy in reality><We saw whether sick or healthy>
+counterSS = 0;
+counterHS = 0;
+counterSH = 0;
+counterHH = 0;
 
-n=10;
-giniIndexUp=0;
-giniIndexDown=0;
+n = 10; % Arbitrary selection of the amount of training data
+
+% GINI index: TODO delete??
+giniIndexUp = 0;
+giniIndexDown = 0;
 for i=1: n
     for j=1:n
-        giniIndexUp = giniIndexUp  + abs( Hx(i)-Hx(j) ) ;        
+        giniIndexUp = giniIndexUp  + abs(Hx(i) - Hx(j)) ;        
     end
-    giniIndexDown= giniIndexDown+Hx(i);
+    giniIndexDown = giniIndexDown + Hx(i);
 end
+giniIndex = giniIndexUp / (2*n*giniIndexDown);
 
-
-found=0;
-sum=0;
-i=1;
-while found<n
-    if data(i,9)==1
+% Finding ____ by Normal Distribution TODO change this comment
+found = 0; % Training data - n first healthy
+sum = 0; % Sum of n first healthy
+i = 1;
+while found < n
+    if data(i,columns) == 1
         sum = sum + Hx(i);
-        found=found+1;
-        disp ("Hx(i)"+Hx(i) +" Sum"+ sum)
+        found = found + 1;
     end
-    i=i+1;
+    i = i + 1;
 end
 
-average = sum/n;
+average = sum / n; % Average of the n first healthy
 
-
-standardDev = 0;
-for x=1:i
-    if data(x,9)==1
-        standardDev = standardDev + power( Hx(x)-average ,2);
+standardDev = 0; % Standard deviation
+for x = 1:i
+    if data(x,columns) == 1
+        standardDev = standardDev + power(Hx(x)-average ,2);
     end
 end
- standardDev = sqrt(standardDev /n);
+standardDev = sqrt(standardDev / n);
 
- 
+% Finding anomalies and the counters:
 for i = 1:length(Hx)
-    if Hx(i) < average+(standardDev*1)
-%         disp(i+1 + " is Anomaly.");
-        
-        if data(i,9)==0
-            counterSS=counterSS+1;
+    if Hx(i) < average - (standardDev * 1)
+        disp(i+1 + " is Anomaly.");
+        if data(i,columns) == 0
+            counterSS = counterSS + 1;
         else 
-            counterHS=counterHS+1;
+            counterHS = counterHS + 1;
         end
     else
-        if data(i,9)==0
-             counterSH=counterSH+1;
+        if data(i,columns) == 0
+             counterSH = counterSH + 1;
         else
-             counterHH=counterHH+1;
+             counterHH = counterHH + 1;
         end
     end 
 end
- 
-good=counterSS+counterHH;
-bad=counterHS+counterSH; 
 
+% In what we were right and wrong:
+good = counterSS + counterHH;
+bad = counterHS + counterSH;
 disp ("We were right in "+ good + " cases" );
 disp ("We were wrong in "+ bad + " cases");
-giniIndex = giniIndexUp / (2*n*giniIndexDown);
+
+% TODO delete all of this:
 % disp (giniIndex);
 % disp ("average"+average);
-%  disp (average+standardDev + "take1 with +");
-%   disp (average+(standardDev*2) + "take2 with +");
-%   disp (average+(standardDev*3) + "take3 with +");
-%   disp (average-standardDev + "take4 with -");
-%   disp (average-(standardDev*2) + "take5 with -");
-%   disp (average-(standardDev*3) + "take6 with -");
-%   
+% disp (average+standardDev + "take1 with +");
+% disp (average+(standardDev*2) + "take2 with +");
+% disp (average+(standardDev*3) + "take3 with +");
+% disp (average-standardDev + "take4 with -");
+% disp (average-(standardDev*2) + "take5 with -");
+% disp (average-(standardDev*3) + "take6 with -");
 clear;
  
 % -------------------------------------------------------------------------
@@ -109,8 +112,7 @@ function p = ProbabilityCalculation(number, maxVal)
         p = number / maxVal;
 end
 
-
-
+% TODO delete all of this:
 % 0.16562take1 with + (268 r  500w)
 % 0.20496take2 with + 268
 % 0.2443take3 with +  268
@@ -119,9 +121,7 @@ end
 % 0.047611take5 with - 538
 % 0.0082735take6 with -533
 
-
-
-%     
+% TODO old version - delete ?    
 %     for j = 1:columns-1
 % 
 %         number = data(i,j);
