aafec75746c82cf4f626e960756930ac093473a4
karinbe <karinbenson8@gmail.com>
2018-05-30 12:08:13 +0300

    update iter4 for Entropy & update Entropy tool

--- Research Channels/Entropy/iter4/entropy.m
+++ Research Channels/Entropy/iter4/entropy.m
@@ -39,18 +39,6 @@ counterHH = 0;
 
 n = 10; % Arbitrary selection of the amount of training data
 
-% % GINI index: TODO delete??
-% giniIndexUp = 0;
-% giniIndexDown = 0;
-% for i=1: n
-%     for j=1:n
-%         giniIndexUp = giniIndexUp  + abs(Hx(i) - Hx(j)) ;        
-%     end
-%     giniIndexDown = giniIndexDown + Hx(i);
-% end
-% giniIndex = giniIndexUp / (2*n*giniIndexDown);
-
-% Finding ____ by Normal Distribution TODO change this comment
 found = 0; % Training data - n first healthy
 sum = 0; % Sum of n first healthy
 i = 1;
@@ -74,7 +62,7 @@ standardDev = sqrt(standardDev / n);
 
 % Finding anomalies and the counters:
 for i = 1:length(Hx)
-    if Hx(i) < average + (standardDev * 3)
+    if Hx(i) < average - (standardDev * 1)
         disp(i+1 + " is Anomaly.");
         if data(i,columns) == 0
             counterSS = counterSS + 1;
@@ -96,15 +84,6 @@ bad = counterHS + counterSH;
 disp ("We were right in "+ good + " cases" );
 disp ("We were wrong in "+ bad + " cases");
 
-% TODO delete all of this:
-% disp (giniIndex);
-% disp ("average"+average);
-% disp (average+standardDev + "take1 with +");
-% disp (average+(standardDev*2) + "take2 with +");
-% disp (average+(standardDev*3) + "take3 with +");
-% disp (average-standardDev + "take4 with -");
-% disp (average-(standardDev*2) + "take5 with -");
-% disp (average-(standardDev*3) + "take6 with -");
 clear;
  
 % -------------------------------------------------------------------------
@@ -113,29 +92,3 @@ clear;
 function p = ProbabilityCalculation(number, maxVal)
         p = number / maxVal;
 end
-
-% TODO delete all of this:
-% 0.16562take1 with + (268 r  500w)
-% 0.20496take2 with + 268
-% 0.2443take3 with +  268
-% 0.1 = >>>> 544
-% 0.086948take4 with - 553
-% 0.047611take5 with - 538
-% 0.0082735take6 with -533
-
-% TODO old version - delete ?    
-%     for j = 1:columns-1
-% 
-%         number = data(i,j);
-%         px = ProbabilityCalculation(number, maxColumnsArray(j));
-%         Hx(i) = Hx(i) + (px * log10(1/px));
-%         
-%     end
-
-%     number = data(i,2);
-%     px = ProbabilityCalculation(number, maxColumnsArray(2));
-%     Hx(i) = px * log10(1/px);
-%     
-%     number = data(i,5);
-%     px = ProbabilityCalculation(number, maxColumnsArray(5));
-%     Hx(i) = Hx(i) + (px * log10(1/px));
