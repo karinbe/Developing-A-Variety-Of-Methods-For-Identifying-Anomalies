10070fa84b1e12321b0e8759de83e8b155a85ebc
Hadas135 <hadasi135@gmail.com>
2018-06-08 18:02:34 +0300

    Majority Vote

---
+++ src/Majority vote/project.m
@@ -0,0 +1,52 @@
+rng default; % For reproducibility
+
+% Import the data:
+[~, ~, raw] = xlsread('C:\Users\Hadas\Desktop\Developing-Variety-Of-Methods-For-Identifying-Anomalies-\tables\Diabeteswith01.xls','Sheet1','A2:I769');
+
+R = cellfun(@(x) (~isnumeric(x) && ~islogical(x)) || isnan(x),raw); % Find non-numeric cells
+raw(R) = {2.0}; % Replace non-numeric cells
+
+% Create output variable:
+data = reshape([raw{:}],size(raw));
+    
+MLarray = ML(data);
+
+entropyArray = entropy(data);
+
+counterSS = 0;
+counterHS = 0;
+counterSH = 0;
+counterHH = 0;
+
+% for i = 1:length(MLarray)
+%     if MLarray(i) + entropyArray(i) >= 2 % will be anomaly for sure
+%         disp("Voting: " + i + " is anomaly.")
+%     end
+% end
+
+for i = 1:length(MLarray)-1
+    if MLarray(i) + entropyArray(i) == 2 % will be sick for sure
+        disp("Voting: " + i + " is anomaly.")
+        if data(i,9) == 0 % TODO cols..
+            counterSS = counterSS + 1;
+        else
+            counterHS = counterHS + 1;
+        end
+    elseif MLarray(i) + entropyArray(i) == 0 % will be healthy for sure
+        if data(i,9) == 0 % TODO cols..
+            counterSH = counterSH + 1;
+        else
+            counterHH = counterHH + 1;
+        end
+    end
+end
+
+% In what we were right and wrong:
+good = counterSS + counterHH;
+bad = counterHS + counterSH;
+disp("counterSS: " + counterSS);
+disp("counterSH: " + counterSH);
+disp("counterHS: " + counterHS);
+disp("counterHH: " + counterHH);
+disp ("We were right in "+ good + " cases" );
+disp ("We were wrong in "+ bad + " cases");
