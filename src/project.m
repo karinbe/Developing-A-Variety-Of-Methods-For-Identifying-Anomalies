6218c9848d9d5c55361f8d0a86cb49ae4a39b0dd
karinbe <karinbenson8@gmail.com>
2018-06-14 16:35:29 +0300

    update the whole 3 methods to the final step of our project

--- src/project.m
+++ src/project.m
@@ -1,46 +1,43 @@
 rng default; % For reproducibility
 
 % Import the data:
-[~, ~, raw] = xlsread('C:\Users\קארין\Desktop\speech-unsupervised.xlsx','speech-unsupervised-ad');
+[~, ~, raw] = xlsread('C:\Users\קארין\Desktop\Developing-Variety-Of-Methods-For-Identifying-Anomalies-\tables\Diabeteswith01.xls','Sheet1','A2:I769');
+
 R = cellfun(@(x) (~isnumeric(x) && ~islogical(x)) || isnan(x),raw); % Find non-numeric cells
 raw(R) = {2.0}; % Replace non-numeric cells
 
 % Create output variable:
 data = reshape([raw{:}],size(raw));
 
-   entropyArray = entropy(data);
+[~, columns] = size(data);
 
-%    MLarray = ML(data);
+entropyArray = entropy(data);
 
-%   LZArray = LZ(data);
+MLarray = ML(data);
+
+LZArray = LZ(data);
 
 counterSS = 0;
 counterHS = 0;
 counterSH = 0;
 counterHH = 0;
 
-% for i = 1:length(MLarray)
-%     if MLarray(i) + entropyArray(i) + LZArray(i) >= 2 % anomaly
-%         disp("Voting: " + i + " is anomaly.")
-%     end
-% end
-
-% for i = 1:length(MLarray)-1
-%     if MLarray(i) + entropyArray(i) + LZArray(i) >= 2 % will be sick for sure
-%         disp(i + " is anomaly.")
-%         if data(i,401) == 0 % TODO cols..
-%             counterSS = counterSS + 1;
-%         else
-%             counterHS = counterHS + 1;
-%         end
-%     else
-%         if data(i,401) == 0 % TODO cols..
-%             counterSH = counterSH + 1;
-%         else
-%             counterHH = counterHH + 1;
-%         end
-%     end
-% end
+for i = 1:length(MLarray)-1
+    if MLarray(i) + entropyArray(i) + LZArray(i) >= 2 % will be sick for sure
+        disp(i + " is anomaly.")
+        if data(i,columns) == 0 % TODO cols..
+            counterSS = counterSS + 1;
+        else
+            counterHS = counterHS + 1;
+        end
+    else
+        if data(i,columns) == 0 % TODO cols..
+            counterSH = counterSH + 1;
+        else
+            counterHH = counterHH + 1;
+        end
+    end
+end
 
 % In what we were right and wrong:
 good = counterSS + counterHH;
